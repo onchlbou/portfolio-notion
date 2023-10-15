6eb87198d517a2f4ffdfdfe1e80b608e55db1b33
nathan <nathan.hue@exalens.com>
2023-10-15 16:24:17 +0100

    add favicon

---
+++ html/components/Favicon.js
@@ -0,0 +1,12 @@
+function Favicon() {
+    return (
+        <>
+            {/* <link rel="icon" href="/favicon.ico" type="image/x-icon"></link> */}
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" sizes="192x192" href="/android-chrome-192x192"></link>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        </>
+    );
+  }
+
+export default Favicon;
